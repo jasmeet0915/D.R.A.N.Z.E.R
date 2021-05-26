f118e3f3d6d801fe6aee9616db7c3a5b0d116e77
amanjeetsingh150 <amanjeetsingh150@gmail.com>
2021-05-26 16:30:00 +0530

    Adds `ClassPoolTransformations` for transforming classpool to list of `CtClass`
    - Adds Truth for assertions

--- dranzer-app/buildSrc/build.gradle.kts
+++ dranzer-app/buildSrc/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 
 repositories {
     jcenter()
+    mavenCentral()
     google()
 }
 
@@ -18,4 +19,5 @@ dependencies {
 
     testImplementation("junit:junit:4.13.2")
     testImplementation("com.nhaarman.mockitokotlin2:mockito-kotlin:2.2.0")
+    testImplementation("com.google.truth:truth:1.1.3")
 }
---
+++ dranzer-app/buildSrc/src/main/java/com/droidsingh/daggertrack/ClassPoolTransformations.kt
@@ -0,0 +1,29 @@
+package com.droidsingh.daggertrack
+
+import com.android.SdkConstants.DOT_CLASS
+import com.android.build.api.transform.TransformInvocation
+import javassist.ClassPool
+import javassist.CtClass
+import java.io.File
+
+internal fun ClassPool.mapToCtClassList(transformInvocation: TransformInvocation): List<CtClass> {
+    val allClassFiles = mutableListOf<CtClass>()
+
+    transformInvocation.inputs.forEach { inputs ->
+        inputs.directoryInputs.forEach { directory ->
+            val inputPath = directory.file.absolutePath
+            directory.file.walkTopDown()
+                .forEach {
+                    if (it.absolutePath.endsWith(DOT_CLASS)) {
+                        val fullyQualifiedClassName = it.absolutePath
+                            .substring(
+                                inputPath.length + 1,
+                                it.absolutePath.length - DOT_CLASS.length
+                            ).replace(File.separator, ".")
+                        allClassFiles += get(fullyQualifiedClassName)
+                    }
+                }
+        }
+    }
+    return allClassFiles.toList()
+}
