642630234a3a29b5b25438ddf3ee7f5fd3f2c450
amanjeetsingh150 <amanjeetsingh150@gmail.com>
2021-05-27 23:54:57 +0530

    Adds functionality to calculate CPU time in `dagger-track-clocks`
    - Calculates CPU time of main thread by default

---
+++ dranzer-app/dagger-track-clocks/src/main/cpp/jni_interface.h
@@ -0,0 +1,11 @@
+//
+// Created by Amanjeet Singh on 27/05/21.
+//
+
+#ifndef DRANZER_JNI_INTERFACE_H
+#define DRANZER_JNI_INTERFACE_H
+
+extern "C" jlong
+Java_com_droidsingh_daggertrack_DaggerTrackClocks_getCpuTime(JNIEnv *env, jclass clazz);
+
+#endif //DRANZER_JNI_INTERFACE_H
