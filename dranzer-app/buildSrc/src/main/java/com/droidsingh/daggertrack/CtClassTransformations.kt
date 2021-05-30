945269d72bb65c044fbdc6220cf17fbdfc73c493
amanjeetsingh150 <amanjeetsingh150@gmail.com>
2021-05-30 17:13:38 +0530

    Deletes the factory logic to add clock tracking

--- dranzer-app/buildSrc/src/main/java/com/droidsingh/daggertrack/CtClassTransformations.kt
+++ dranzer-app/buildSrc/src/main/java/com/droidsingh/daggertrack/CtClassTransformations.kt
@@ -1,16 +1,6 @@
 package com.droidsingh.daggertrack
 
-import com.droidsingh.daggertrack.models.DaggerFactoryClass
 import javassist.CtClass
-
-internal const val DAGGER_FACTORY = "dagger.internal.Factory"
-
-internal fun List<CtClass>.mapToDaggerFactoryClass(): List<DaggerFactoryClass> {
-    val daggerCtClasses = filter { it.superclass?.name == DAGGER_FACTORY }
-    return daggerCtClasses.map {
-        DaggerFactoryClass(it.name, it.methods)
-    }
-}
 
 internal fun List<CtClass>.filterDaggerComponents(): List<CtClass> {
     return filter {
